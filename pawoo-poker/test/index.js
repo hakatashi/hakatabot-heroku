5936985498584309e4a5d92aec72f7c8c709a8b7
Koki Takahashi <hakatasiloving@gmail.com>
2017-04-19 16:21:51 +0900

    Add first test code

---
+++ pawoo-poker/test/index.js
@@ -0,0 +1,51 @@
+const mockery = require('mockery');
+const path = require('path');
+const {expect} = require('chai');
+
+describe('pawoo-poker', () => {
+	let callback = null;
+
+	before(() => {
+		mockery.registerMock('./pawoo.js', {
+			toot: (...args) => {
+				if (typeof callback === 'function') {
+					callback(...args);
+				}
+			},
+		});
+	});
+
+	beforeEach(() => {
+		callback = null;
+		mockery.enable({
+			warnOnUnregistered: false,
+		});
+	});
+
+	afterEach(() => {
+		delete require.cache[path.resolve(__dirname, '../index.js')];
+		mockery.disable();
+	});
+
+	it('toots something', () => {
+		process.env.PAWOO_POKER_TOKEN = 'hoge';
+
+		const promise = new Promise((resolve) => {
+			callback = ({access_token, status, visibility}) => {
+				expect(access_token).to.equal('hoge');
+				expect(status).to.be.a('string');
+				expect(status).to.have.string('ドロー！');
+				expect(status).to.have.string('人人人');
+				expect(status).to.have.string('出来役');
+				expect(visibility).to.equal('public');
+
+				delete process.env.PAWOO_POKER_TOKEN;
+				resolve();
+			};
+		});
+
+		require('../index.js');
+
+		return promise;
+	});
+});
