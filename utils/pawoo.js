34abea1fd71f525925cddb842c69162a5dd01932
Koki Takahashi <hakatasiloving@gmail.com>
2017-04-22 01:33:43 +0900

    Implement file upload function in pawoo.toot

--- utils/pawoo.js
+++ utils/pawoo.js
@@ -1,13 +1,31 @@
 const Masto = require('mastodon');
 
-module.exports.toot = ({access_token, status, visibility}) => {
+module.exports.toot = async ({access_token, status, visibility, file}) => {
 	const masto = new Masto({
 		access_token,
 		api_url: 'https://pawoo.net/api/v1/',
 	});
 
-	return masto.post('statuses', {
+	if (!file) {
+		return await masto.post('statuses', {
+			status,
+			visibility,
+		});
+	}
+
+	const response = await masto.post('media', {
+		file: {
+			value: file,
+			options: {
+				filename: 'test.png',
+				contentType: 'image/png',
+			},
+		},
+	});
+
+	return await masto.post('statuses', {
 		status,
 		visibility,
+		media_ids: [response.data.id],
 	});
 };
