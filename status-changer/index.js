ae605260a8dbabfddbf63e3cc2b9987e7d61ddc8
Koki Takahashi <hakatasiloving@gmail.com>
2018-06-12 10:52:08 +0900

    Implement automatic status name updater

--- status-changer/index.js
+++ status-changer/index.js
@@ -3,8 +3,42 @@ const download = require('download');
 const {WebClient} = require('@slack/client');
 const emojiData = require('emoji-data');
 const sample = require('lodash/sample');
-
+const inRange = require('lodash/inRange');
+const unicodeNames = require('unicode-11.0.0/Names');
 const getWaka = require('./get-waka.js');
+
+const derivedNames = [
+	{start: 0x3400, end: 0x4DB5},
+	{start: 0x4E00, end: 0x9FEA},
+	{start: 0xF900, end: 0xFA6D},
+	{start: 0xFA70, end: 0xFAD9},
+	{start: 0x17000, end: 0x187EC},
+	{start: 0x1B170, end: 0x1B2FB},
+	{start: 0x20000, end: 0x2A6D6},
+	{start: 0x2A700, end: 0x2B734},
+	{start: 0x2B740, end: 0x2B81D},
+	{start: 0x2B820, end: 0x2CEA1},
+	{start: 0x2CEB0, end: 0x2EBE0},
+	{start: 0x2F800, end: 0x2FA1D},
+];
+
+const unicodes = [...unicodeNames.entries()].filter(([codepoint, name]) => {
+	if (name.includes('Private Use')) {
+		return false;
+	}
+
+	if (name.includes('Hangul Syllable')) {
+		return false;
+	}
+
+	for (const {start, end} of derivedNames) {
+		if (inRange(codepoint, start, end + 1)) {
+			return false;
+		}
+	}
+
+	return true;
+});
 
 if (require.main === module) {
 	dotenv.config();
@@ -37,16 +71,20 @@ module.exports = (async () => {
 		const statusEmoji = `:${sample(totalEmojis)}:`;
 
 		const waka = await getWaka();
+		const [unicodePoint, unicodeName] = sample(unicodes);
+		const name = `U-${unicodePoint.toString(16).toUpperCase().padStart(4, '0')} ${unicodeName}`;
 
 		await slack.users.profile.set({
 			profile: JSON.stringify({
 				title: waka,
 				status_text: statusText,
 				status_emoji: statusEmoji,
+				real_name: name,
 			}),
 		});
 
 		console.log(`New status: ${statusEmoji} ${statusText}`);
 		console.log(`New title: ${waka}`);
+		console.log(`New name: ${name}`);
 	}
 })();
